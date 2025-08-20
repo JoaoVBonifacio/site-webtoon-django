d56ca9e3f34b37768edefadf4e88cb9e4827898b
JoaoVBonifácio <jboni2014@gmail.com>
2025-08-20 16:38:32 -0300

    Fix: settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.getenv('https://site-webtoon-django2.vercel.app/', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
