d7c6f123a19ddb0932b2b9f0b3ece4ca9665638a
JoaoVBonifácio <jboni2014@gmail.com>
2025-08-22 15:13:33 -0300

    Fix: Limpa wsgi.py para usar configuração de storage do WhiteNoise

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,15 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
-# config/wsgi.py (VERSÃO NOVA E CORRIGIDA)
+# config/wsgi.py (VERSÃO FINAL E CORRETA)
 import os
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-# Pega a aplicação Django padrão
 application = get_wsgi_application()
-
-# Importa o WhiteNoise e "envelopa" a aplicação com ele
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
