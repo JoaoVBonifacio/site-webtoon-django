c35fe611c926415ec1479f4ef194fd75b538a0cd
JoaoVBonifácio <jboni2014@gmail.com>
2025-08-22 15:05:29 -0300

    Fix: Aplica configuração robusta do WhiteNoise no wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# config/wsgi.py (VERSÃO NOVA E CORRIGIDA)
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Pega a aplicação Django padrão
 application = get_wsgi_application()
+
+# Importa o WhiteNoise e "envelopa" a aplicação com ele
+from whitenoise import WhiteNoise
+application = WhiteNoise(application)
